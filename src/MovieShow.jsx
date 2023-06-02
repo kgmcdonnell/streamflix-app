f6c453f468a7326e63ab550612506bce94192969
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-06-02 13:40:05 -0400

    styled movie show and created function to convert runtime to hours and minutes

--- src/MovieShow.jsx
+++ src/MovieShow.jsx
@@ -3,55 +3,59 @@ import ReactPlayer from "react-player/youtube";
 export function MovieShow(props) {
   console.log(props);
 
+  const movieDuration = runtime => {
+    const duration = runtime / 60.0;
+    const hours = Math.floor(duration);
+    const minutes = Math.round(duration - hours);
+    return hours + "h" + " " + minutes + "m";
+  };
+
   return (
     <div id="movie-show">
-      {/* Movie Trailer */}
       {props.movieDetails && props.movieDetails.videos?.results.length > 0 ? (
-        <ReactPlayer
-          className="youtube-videos"
-          url={`https://www.youtube.com/embed/${props.movieDetails.videos.results[0].key}`}
-        />
+        <div className="center-video">
+          <ReactPlayer
+            className="youtube-videos"
+            url={`https://www.youtube.com/embed/${props.movieDetails.videos.results[0].key}`}
+            playing={true}
+          />
+        </div>
       ) : (
         <></>
       )}
-
-      {/* Movie Title */}
-      <h3>{props.movie.original_title}</h3>
-      {/* Movie Overview */}
-      <p>
-        <small>{props.movie.overview}</small>
-      </p>
-      {/* Release Date */}
-      <p>
-        <small>Release Date: {props.movieDetails.release_date}</small>
-      </p>
-      {/* Genres */}
-      <small>
-        Genres:
-        {props.movieDetails && props.movieDetails.genres?.length > 0 ? (
-          props.movieDetails.genres.map(genre => (
-            <div key={genre.id}>
-              <p style={{ margin: "0" }}>{genre.name}</p>
-            </div>
-          ))
-        ) : (
-          <></>
-        )}
-      </small>
-      <div style={{ marginTop: "10px" }}>
-        <small>
-          Cast:
-          {/* Cast */}
-          {props.movieDetails && props.movieDetails.credits?.cast.length > 0 ? (
-            props.movieDetails.credits.cast.slice(0, 6).map(member => (
-              <div key={member.id}>
-                <p style={{ margin: "0" }}>{member.name}</p>
-              </div>
-            ))
-          ) : (
-            <></>
-          )}
-        </small>
+      <div className="row">
+        <div className="col-sm-7">
+          <p>
+            <small>
+              {props.movieDetails.release_date} | {movieDuration(props.movieDetails.runtime)}
+            </small>
+          </p>
+          <p>
+            <small>{props.movie.overview}</small>
+          </p>
+        </div>
+        <div className="col-sm-5">
+          <small>
+            Genres:
+            {props.movieDetails && props.movieDetails.genres?.length > 0 ? (
+              props.movieDetails.genres.map(genre => <div key={genre.id}>{genre.name}</div>)
+            ) : (
+              <></>
+            )}
+          </small>
+          <small>
+            Cast:
+            {props.movieDetails && props.movieDetails.credits?.cast.length > 0 ? (
+              props.movieDetails.credits.cast.slice(0, 6).map(member => (
+                <div key={member.id}>
+                  <p style={{ margin: "0" }}>{member.name}</p>
+                </div>
+              ))
+            ) : (
+              <></>
+            )}
+          </small>
+        </div>
       </div>
     </div>
   );
