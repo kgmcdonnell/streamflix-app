1136ca6355f1cbab5057a9caa54d3103f8f09464
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-06-13 22:22:49 -0400

    moved movies index styling into css stylesheet

--- src/MovieShow.jsx
+++ src/MovieShow.jsx
@@ -1,19 +1,20 @@
 import "./MovieShow.scoped.scss";
 import ReactPlayer from "react-player/youtube";
+
 export function MovieShow(props) {
   console.log(props);
 
-  const movieDuration = runtime => {
+  const movieInformation = (runtime, release) => {
     const duration = runtime / 60.0;
     const hours = Math.floor(duration);
     const minutes = Math.round(duration - hours);
-    return hours + "h" + " " + minutes + "m";
+    return release + " " + hours + "h" + " " + minutes + "m";
   };
 
   return (
     <div id="movie-show">
       {props.movieDetails && props.movieDetails.videos?.results.length > 0 ? (
-        <div className="center-video">
+        <div id="slider" className="center-video">
           <ReactPlayer
             className="youtube-videos"
             url={`https://www.youtube.com/embed/${props.movieDetails.videos.results[0].key}`}
@@ -24,28 +25,26 @@ export function MovieShow(props) {
         <></>
       )}
       <div className="row">
-        <div className="col-sm-6">
-          <p>
-            <small>
-              {props.movieDetails.release_date} | {movieDuration(props.movieDetails.runtime)}
-            </small>
+        <div className="col-sm-7">
+          <p style={{ color: "rgb(224,224,224)" }}>
+            {movieInformation(props.movieDetails.runtime, props.movieDetails.release_date.split("-")[0])}
           </p>
-          <p>
+          <p style={{ marginTop: "20px" }}>
             <small>{props.movie.overview}</small>
           </p>
         </div>
-        <div className="col-sm-6">
-          <div className="row" style={{ paddingTop: "0" }}>
+        <div className="col-sm-5">
+          <div className="row" style={{ paddingTop: "0px" }}>
             <small>
               {props.movieDetails && props.movieDetails.credits?.cast.length > 0 ? (
                 props.movieDetails.credits.cast.slice(0, 6).map((member, index) => (
                   <div key={member.id}>
                     {index === 0 ? (
-                      <p style={{ margin: "0", float: "left" }}>
+                      <p>
                         <span style={{ color: "grey" }}>Cast:</span> {member.name},{" "}
                       </p>
                     ) : (
-                      <p style={{ margin: "0", float: "left" }}>{index < 5 ? member.name + ", " : member.name}</p>
+                      <p>{index < 5 ? member.name + ", " : member.name}</p>
                     )}
                   </div>
                 ))
@@ -60,14 +59,12 @@ export function MovieShow(props) {
                 props.movieDetails.genres.map((genre, index) => (
                   <div key={genre.id}>
                     {index === 0 ? (
-                      <p style={{ margin: "0", float: "left" }}>
+                      <p>
                         <span style={{ color: "grey" }}>Genres: </span>
                         {genre.name},{" "}
                       </p>
                     ) : (
-                      <p style={{ margin: "0", float: "left" }}>
-                        {index !== props.movieDetails.genres.length - 1 ? genre.name + ", " : genre.name}
-                      </p>
+                      <p>{index !== props.movieDetails.genres.length - 1 ? genre.name + ", " : genre.name}</p>
                     )}
                   </div>
                 ))
--- src/MoviesIndex.jsx
+++ src/MoviesIndex.jsx
@@ -5,16 +5,13 @@ export function MoviesIndex(props) {
   return (
     <div className="movies-index">
       <h5>Action</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left" }}>
+      <div className="movies">
         {props.actionMovie && props.actionMovie.results?.length > 0 ? (
           props.actionMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{
                     backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')`,
                   }}
@@ -29,16 +26,13 @@ export function MoviesIndex(props) {
       </div>
 
       <h5>Adventure</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left" }}>
+      <div className="movies">
         {props.adventureMovie && props.adventureMovie.results?.length > 0 ? (
           props.adventureMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')` }}
                   onClick={() => props.onShowMovie(movie)}
                 ></div>
@@ -50,16 +44,13 @@ export function MoviesIndex(props) {
         )}
       </div>
       <h5>Romance</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left" }}>
+      <div className="movies">
         {props.romanceMovie && props.romanceMovie.results?.length > 0 ? (
           props.romanceMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')` }}
                   onClick={() => props.onShowMovie(movie)}
                 ></div>
@@ -71,16 +62,13 @@ export function MoviesIndex(props) {
         )}
       </div>
       <h5>Comedy</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left" }}>
+      <div className="movies">
         {props.comedyMovie && props.comedyMovie.results?.length > 0 ? (
           props.comedyMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')` }}
                   onClick={() => props.onShowMovie(movie)}
                 ></div>
@@ -92,16 +80,13 @@ export function MoviesIndex(props) {
         )}
       </div>
       <h5>Family Movies</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left" }}>
+      <div className="movies">
         {props.familyMovie && props.familyMovie.results?.length > 0 ? (
           props.familyMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')` }}
                   onClick={() => props.onShowMovie(movie)}
                 ></div>
@@ -113,16 +98,13 @@ export function MoviesIndex(props) {
         )}
       </div>
       <h5>Animation</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left", paddingBottom: "50px" }}>
+      <div className="movies">
         {props.animationMovie && props.animationMovie.results?.length > 0 ? (
           props.animationMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')` }}
                   onClick={() => props.onShowMovie(movie)}
                 ></div>
@@ -134,16 +116,13 @@ export function MoviesIndex(props) {
         )}
       </div>
       <h5>Horror</h5>
-      <div className="movies" style={{ display: "flex", justifyContent: "left", paddingBottom: "50px" }}>
+      <div className="movies">
         {props.horrorMovie && props.horrorMovie.results?.length > 0 ? (
           props.horrorMovie.results.map(movie => (
             <div key={movie.id}>
-              <div
-                className="card border-dark"
-                style={{ width: "15rem", height: "22rem", overflow: "hidden", marginRight: "10px" }}
-              >
+              <div className="card border-dark">
                 <div
-                  className="card-body movie-card"
+                  className="card-body"
                   style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')` }}
                   onClick={() => props.onShowMovie(movie)}
                 ></div>
